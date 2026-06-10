796666614b93a7c7d76f18576ee0703b08c2e261
vinspdb <57073656+vinspdb@users.noreply.github.com>
2026-06-10 15:51:00 +0200

    Add files via upload

---
+++ pydantic/extract_ocel.py
@@ -0,0 +1,21 @@
+from pystackt import get_github_log, export_to_ocel2
+
+#please, insert your GITHUB_ACCESS_TOKEN
+
+get_github_log(
+    GITHUB_ACCESS_TOKEN="your_github_token",
+    repo_owner="pydantic",
+    repo_name="pydantic",
+    max_issues=2500,
+    save_after_num_issues=500,
+    quack_db="ocel/pydantic.duckdb"
+)
+
+export_to_ocel2(
+    quack_db="ocel/pydantic.duckdb",
+    schema_in="main",
+    schema_out="ocel2",
+    sqlite_db="ocel/pydantic.sqlite"
+)
+
+print("Done!")
